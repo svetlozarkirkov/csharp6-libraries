26c146d27eb2c2b3ccb442746474635ba09dc6a8
Svetlozar Kirkov <svetlozark@gmail.com>
2016-04-10 18:07:11 +0300

    working on arraystack tests, fixed arraystack concrete constructors

--- Collections.Tests/Stack/Core/Base/ArrayStackBaseTests.cs
+++ Collections.Tests/Stack/Core/Base/ArrayStackBaseTests.cs
@@ -5,40 +5,82 @@
     using Moq;
     using NUnit.Framework;
     using Collections.Stack.Core.Base;
+    using Collections.Stack.ExceptionHandling.Core.Concrete;
 
     [TestFixture]
     public class ArrayStackBaseTests
     {
         /// <summary>
-        /// When the stack is initialized - the size must be '0'
+        /// When the stack is initialized with an invalid capacity
+        /// Should throw "InvalidCapacityGivenException"
         /// </summary>
+        /// <exception cref="Exception">A delegate callback throws an exception.</exception>
         [Test]
-        public void StackIsInitialized_SizeMustBeZero()
+        public void StackIsInitialized_InvalidCapacityGiven_ShouldThrowException()
         {
-            // Act
-            var classMock = new Mock<ArrayStackBase<object>> {CallBase = true};
-
-            // Assert
-            classMock.Object.Size().Should().Be(0);
         }
 
         /// <summary>
-        /// When the stack is initialized and an item is pushed - the size after must be '1'
+        /// When the stack is initialized
+        /// The size must be '0'
         /// </summary>
-        /// <exception cref="AggregateException">The exception that contains all the individual exceptions thrown on all threads.</exception>
-        /// <exception cref="ArgumentNullException">The argument is null.</exception>
-        /// <exception cref="OverflowException">The array is multidimensional and contains more than <see cref="F:System.Int32.MaxValue" /> elements.</exception>
         [Test]
-        public void StackIsInitialized_ItemIsPushed_SizeMustBeOne()
+        public void WhenEmptyStack_SizeMustBeZero()
         {
-            // Arrange
-            var classMock = new Mock<ArrayStackBase<object>> {CallBase = true};
-
             // Act
-            classMock.Object.Push(It.IsAny<object>());
+            var mock = new Mock<ArrayStackBase<object>> { CallBase = true };
 
             // Assert
-            classMock.Object.Size().Should().Be(1);
+            mock.Object.Size().Should().Be(0);
+        }
+
+        /// <summary>
+        /// When the stack is initialized
+        /// "Pop" method should throw "EmptyStackException"
+        /// </summary>
+        /// <exception cref="EmptyStackException">The stack is empty.</exception>
+        [Test]
+        public void EmptyStack_Pop_ShouldThrowException()
+        {
+            // Arrange
+            var mock = new Mock<ArrayStackBase<object>> { CallBase = true };
+
+            // Act Assert
+            mock.Object.Invoking(c => c.Pop()).ShouldThrow<EmptyStackException>();
+        }
+
+        /// <summary>
+        /// When the stack is initialized
+        /// "Peek" method should throw "EmptyStackException"
+        /// </summary>
+        /// <exception cref="EmptyStackException">The stack is empty.</exception>
+        [Test]
+        public void EmptyStack_Peek_ShouldThrowException()
+        {
+            // Arrange
+            var mock = new Mock<ArrayStackBase<object>> { CallBase = true };
+
+            // Act Assert
+            mock.Object.Invoking(c => c.Peek()).ShouldThrow<EmptyStackException>();
+        }
+
+        /// <summary>
+        /// When the stack is initialized and an item is pushed
+        /// The size of the stack must be '1'
+        /// </summary>
+        /// <exception cref="AggregateException">The exception that contains all the individual exceptions thrown on all threads.</exception>
+        /// <exception cref="OverflowException">The array is multidimensional and contains more than <see cref="F:System.Int32.MaxValue" /> elements.</exception>
+        [Test]
+        public void EmptyStack_WhenItemIsPushed_SizeMustBeOne()
+        {
+            // Arrange
+            var mock = new Mock<ArrayStackBase<object>> {CallBase = true};
+
+            // Act
+            mock.Object.Push(It.IsAny<object>());
+
+            // Assert
+            mock.Object.Size().Should().Be(1);
         }
     }
 }
--- Collections/Stack/Core/Concrete/ArrayStack.cs
+++ Collections/Stack/Core/Concrete/ArrayStack.cs
@@ -1,6 +1,7 @@
 namespace Collections.Stack.Core.Concrete
 {
     using Collections.Stack.Core.Base;
+    using Collections.Stack.ExceptionHandling.Core.Concrete;
 
     /// <summary>
     /// Default implementation of ArrayStackBase.
@@ -8,5 +9,21 @@ namespace Collections.Stack.Core.Concrete
     /// <typeparam name="T">The type of items in the stack.</typeparam>
     public class ArrayStack<T> : ArrayStackBase<T>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArrayStack{T}"/> class.
+        /// </summary>
+        /// <exception cref="InvalidStackCapacityGivenException">Capacity is less than zero.</exception>
+        public ArrayStack()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArrayStack{T}"/> class.
+        /// </summary>
+        /// <param name="capacity">The initial capacity.</param>
+        /// <exception cref="InvalidStackCapacityGivenException">Capacity is less than or equal to zero.</exception>
+        public ArrayStack(int capacity) : base(capacity)
+        {
+        }
     }
 }

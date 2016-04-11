4b440e847710244fb11f4ea28bcd66461e656e61
Svetlozar Kirkov <svetlozark@gmail.com>
2016-04-11 15:31:38 +0300

    more tests in stack collections

---
+++ Collections.Tests/Stack/Core/Concrete/ArrayStackTests.cs
@@ -0,0 +1,71 @@
+﻿namespace Collections.Tests.Stack.Core.Concrete
+{
+    using System;
+
+    using NUnit.Framework;
+    using Collections.Stack.Core.Concrete;
+    using Collections.Stack.ExceptionHandling.Core.Concrete;
+
+    using FluentAssertions;
+
+    [TestFixture]
+    public class ArrayStackTests
+    {
+        private class ArrayStackStub<T> : ArrayStack<T>
+        {
+            /// <exception cref="!:InvalidStackCapacityGivenException">Capacity is less than zero.</exception>
+            public ArrayStackStub()
+            {
+            }
+
+            /// <exception cref="InvalidStackCapacityGivenException">Capacity is less than or equal to zero.</exception>
+            public ArrayStackStub(int capacity) : base(capacity)
+            {
+            }
+        }
+
+        /// <summary>
+        /// The default parameterless constructor should not throw the specific exception.
+        /// </summary>
+        /// <exception cref="Exception">A delegate callback throws an exception.</exception>
+        [Test]
+        public void DefaultParameterlessConstructor_ShouldNotThrowException()
+        {
+            // Arrange
+            Action act = () => new ArrayStackStub<object>();
+
+            // Act Assert
+            act.ShouldNotThrow<InvalidStackCapacityGivenException>();
+        }
+
+        /// <summary>
+        /// Constructor with capacity - if invalid capacity is given (0) - throw exception
+        /// </summary>
+        /// <exception cref="InvalidStackCapacityGivenException">Capacity is less than or equal to zero.</exception>
+        /// <exception cref="Exception">A delegate callback throws an exception.</exception>
+        [Test]
+        public void ConstructorWithCapacity_InvalidCapacity_ShouldThrowException()
+        {
+            // Arrange
+            Action act = () => new ArrayStackStub<object>(0);
+
+            // Act Assert
+            act.ShouldThrowExactly<InvalidStackCapacityGivenException>();
+        }
+
+        /// <summary>
+        /// Constructor with capacity - if valid capacity is given (1) - exception should not be thrown
+        /// </summary>
+        /// <exception cref="InvalidStackCapacityGivenException">Capacity is less than or equal to zero.</exception>
+        /// <exception cref="Exception">A delegate callback throws an exception.</exception>
+        [Test]
+        public void ConstructorWithCapacity_ValidCapacity_ShouldNotThrowException()
+        {
+            // Arrange
+            Action act = () => new ArrayStackStub<object>(1);
+
+            // Act Assert
+            act.ShouldNotThrow<InvalidStackCapacityGivenException>();
+        }
+    }
+}
---
+++ Collections.Tests/Stack/Core/Concrete/PermanentArrayStackTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Collections.Tests.Stack.Core.Concrete
+{
+    using System;
+
+    using NUnit.Framework;
+    using Collections.Stack.Core.Concrete;
+    using Collections.Stack.ExceptionHandling.Core.Concrete;
+
+    using FluentAssertions;
+
+    using Moq;
+
+    [TestFixture]
+    public class PermanentArrayStackTests
+    {
+        /// <summary>
+        /// Stub class
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <seealso cref="Collections.Stack.Core.Concrete.PermanentArrayStack{T}" />
+        private class PermanentArrayStackStub<T> : PermanentArrayStack<T>
+        {
+            /// <exception cref="InvalidStackCapacityGivenException">The given capacity is less than or equal to zero.</exception>
+            public PermanentArrayStackStub(int capacity) : base(capacity)
+            {
+            }
+        }
+
+        /// <summary>
+        /// The stack is initialized with capacity (1), if second item is pushed - throw the specific exception
+        /// </summary>
+        /// <exception cref="AggregateException">The exception that contains all the individual exceptions thrown on all threads.</exception>
+        /// <exception cref="OverflowException">The array is multidimensional and contains more than <see cref="F:System.Int32.MaxValue" /> elements.</exception>
+        /// <exception cref="InvalidStackCapacityGivenException">The given capacity is less than or equal to zero.</exception>
+        [Test]
+        public void StackIsInitialized_FullCapacityIsReached_IfPush_ThrowException()
+        {
+            // Arrange
+            var stub = new PermanentArrayStackStub<object>(1);
+            stub.Push(It.IsAny<object>());
+
+            // Act Assert
+            stub.Invoking(s => s.Push(It.IsAny<object>()))
+                .ShouldThrowExactly<FullStackException>();
+        }
+    }
+}

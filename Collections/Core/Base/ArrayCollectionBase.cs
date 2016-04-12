f1a7055970b4f3c2b612b6fda7631cd3d0d982c5
Svetlozar Kirkov <svetlozark@gmail.com>
2016-04-12 15:51:09 +0300

    arraycollection abstraction

--- Collections/Core/Base/ArrayCollectionBase.cs
+++ Collections/Core/Base/ArrayCollectionBase.cs
@@ -1,5 +1,6 @@
 ﻿namespace Collections.Core.Base
 {
+    using Collections.Core.ExceptionHandling.Concrete;
     using Collections.Core.Interface;
 
     /// <summary>
@@ -9,5 +10,53 @@
     /// <seealso cref="ICollection" />
     public abstract class ArrayCollectionBase<T> : ICollection
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArrayCollectionBase{T}"/> class.
+        /// </summary>
+        /// <param name="capacity">The capacity.</param>
+        /// <exception cref="InvalidCollectionCapacityException">The given capacity is less than or equal to zero.</exception>
+        protected ArrayCollectionBase(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new InvalidCollectionCapacityException();
+            }
+
+            this.Collection = new T[capacity];
+            this.InitializedCapacity = capacity;
+            this.CurrentPosition = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArrayCollectionBase{T}"/> class.
+        /// </summary>
+        /// <exception cref="InvalidCollectionCapacityException">The given capacity is less than or equal to zero.</exception>
+        protected ArrayCollectionBase() : this(DefaultCapacity)
+        {
+        }
+
+        /// <summary>
+        /// Gets the default capacity.
+        /// </summary>
+        /// <value>The default capacity.</value>
+        protected static int DefaultCapacity => 8;
+
+        /// <summary>
+        /// Gets or sets the collection.
+        /// </summary>
+        /// <value>The collection.</value>
+        protected T[] Collection { get; set; }
+
+        /// <summary>
+        /// Gets the initialized capacity.
+        /// </summary>
+        /// <value>The initialized capacity.</value>
+        protected int InitializedCapacity { get; }
+
+        /// <summary>
+        /// Gets or sets the current position.
+        /// </summary>
+        /// <value>The current position.</value>
+        protected int CurrentPosition { get; set; }
     }
 }
---
+++ Collections/Core/ExceptionHandling/Concrete/InvalidCollectionCapacityException.cs
@@ -0,0 +1,70 @@
+﻿namespace Collections.Core.ExceptionHandling.Concrete
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Class InvalidCollectionCapacityException.
+    /// </summary>
+    /// <seealso cref="System.ArgumentOutOfRangeException" />
+    [Serializable]
+    public class InvalidCollectionCapacityException : ArgumentOutOfRangeException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCollectionCapacityException"/> class.
+        /// </summary>
+        internal InvalidCollectionCapacityException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCollectionCapacityException"/> class.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter that causes this exception.</param>
+        internal InvalidCollectionCapacityException(string paramName) : base(paramName)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCollectionCapacityException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for this exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        internal InvalidCollectionCapacityException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCollectionCapacityException"/> class.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <param name="message">The message that describes the error.</param>
+        internal InvalidCollectionCapacityException(string paramName, string message)
+            : base(paramName, message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCollectionCapacityException"/> class.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter that caused the exception.</param>
+        /// <param name="actualValue">The value of the argument that causes this exception.</param>
+        /// <param name="message">The message that describes the error.</param>
+        internal InvalidCollectionCapacityException
+            (string paramName, object actualValue, string message)
+            : base(paramName, actualValue, message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidCollectionCapacityException"/> class.
+        /// </summary>
+        /// <param name="info">The object that holds the serialized object data.</param>
+        /// <param name="context">An object that describes the source or destination of the serialized data.</param>
+        protected InvalidCollectionCapacityException
+            (SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
